7a7f5d2425185a767ca1675b8c65d0d441fb9138
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2018-04-13 19:24:32 -0300

    Exemplo de interseccao de segmentos com raios saindo de um ponto

--- math_utils.py
+++ math_utils.py
@@ -9,6 +9,11 @@ def dist_sq(p1, p2):
     return (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
 
 
+def dist(p1, p2):
+    """Distance between points p1 and p2"""
+    return math.sqrt(dist_sq(p1, p2))
+
+
 def my_atan2(y, x):
     """atan2 in range [0, 2*pi]."""
     theta = math.atan2(y, x)
